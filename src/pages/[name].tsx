4e54aec1cabae8f406ff4e496bce95797c36bb7e
Johiny <johiny7@live.com>
2023-04-11 12:38:14 -0500

    mini proyecto completado

---
+++ src/pages/[name].tsx
@@ -0,0 +1,76 @@
+import styled from "styled-components"
+import leaf from "@/media/leaf.png"
+import Image from 'next/image'
+import { keyframes } from "styled-components"
+
+const entranceAnimation = keyframes`
+  0% {
+    -webkit-transform: translateY(50px);
+            transform: translateY(50px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+            transform: translateY(0);
+    opacity: 1;
+  }
+`
+
+const StyleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  justify-content: center;
+  gap: 2vh;
+  h2{
+    animation: ${entranceAnimation} 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+  }
+`
+const StyleAdvice = styled.p`
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 600;
+    font-size: x-large;
+    width: 40%;
+    text-align: center;
+    animation: ${entranceAnimation} 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+`
+const Leaf = styled.div`
+    position: absolute;
+    img {filter: invert(61%) sepia(4%) saturate(1791%) hue-rotate(349deg) brightness(95%) contrast(82%);}
+    top: 35vh;
+    transform: rotate(12deg);
+    animation: ${entranceAnimation} 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+`
+const DailyAdvice = (props : any) => {
+  return (
+    <StyleContainer>
+        <Leaf>
+        <Image src={leaf} width={80} height={80} alt='wisdom tree image'/>
+        </Leaf>
+        <h2>Hi {props.name} for your today advice:</h2>
+        <StyleAdvice><q>{props.advice}</q></StyleAdvice>
+    </StyleContainer>
+  )
+}
+
+export async function getStaticPaths(){
+    return{
+        paths: [],
+        fallback: "blocking"
+    }
+}
+
+export async function getStaticProps({params} : any){
+    const res = await fetch('https://api.adviceslip.com/advice') 
+    const advice= await res.json()
+    return{
+        props:{
+            advice: advice.slip.advice.replace('"',""),
+            name : params.name
+        },
+        revalidate: 86400,
+    }
+}
+
+export default DailyAdvice
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,51 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styled from 'styled-components'
+import tree from '@/media/tree.svg'
+import { useRouter } from 'next/router'
 
-const inter = Inter({ subsets: ['latin'] })
-
+const StyleTitle =  styled.h2`
+  font-size: large;
+  font-family: Arial, Helvetica, sans-serif;
+`
+const StyleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+  justify-content: center;
+`
+const StyledInput = styled.input`
+  padding: 2vh 2vh;
+  background-color: #A4907C;
+  border: none;
+  text-align: center;
+  color: #F1DEC9;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 500;
+  font-size: 1rem;
+  border-radius: 5px;
+  width: 24vw;
+  ::placeholder{
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 500;
+    color: #F1DEC9;
+  }
+  :focus{
+    outline: none;
+  }
+`
+const StyledImg = styled.div`
+  img{
+    filter: invert(61%) sepia(4%) saturate(1791%) hue-rotate(349deg) brightness(95%) contrast(82%);
+  }
+`
 export default function Home() {
+  const router = useRouter()
+
+  const navTo = (name : string) => {
+    router.push(`/${name}`)
+  }
   return (
     <>
       <Head>
@@ -14,101 +54,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <StyleContainer>
+        <StyledImg>
+        <Image src={tree} width={300} height={300} alt='wisdom tree image'/>
+        </StyledImg>
+      <StyleTitle>Get Your Daily Advice</StyleTitle>
+      <StyledInput onKeyUp={(e) => {
+        if(e.key === 'Enter'){
+          navTo(e.currentTarget.value)
+        }
+      }} placeholder='Enter your name and press
+     enter'/>
+      </StyleContainer>
     </>
   )
 }
